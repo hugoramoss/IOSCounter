6ac1dd4149f12a68c2e1434ed0a1fe2fa45ec1b3
carioca715 <hugo.ramos95@hotmail.com>
2026-07-30 09:41:02 +0100

    Update ContentView.swift
    A simple counter app built with SwiftUI.

--- IOSCounter/ContentView.swift
+++ IOSCounter/ContentView.swift
@@ -8,14 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var count: Int  = 0
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        
+        VStack{
+            Text("Count up")
+            Text("\(count)").font(.system(size: 30, weight: .bold))
+            if count == 10
+            {
+                Text("Congratulations!🎉")
+            } else if count == 20 {
+                Text("This is the maximum")
+            }
+            
+            
+            
         }
-        .padding()
+        HStack{
+            Button("Decrement") {
+                if count == 0 {
+                    
+                } else {
+                    count -= 1
+                }
+            }
+            
+            Button("Increment") {
+                count += 1
+                if count > 20 {
+                    count = 0
+                }
+            }
+        }
+        VStack{
+            Button("Reset") {
+                count = 0
+            }
+        }
     }
 }
 
